c402bb2e07f343a07967277f758f02042b9a02d8
nagasaki <nagasaki@atf.co.jp>
2021-06-14 23:30:37 +0900

    update comonn A1 1

--- zikkenn1/common.java
+++ zikkenn1/common.java
@@ -1,5 +1,5 @@
 public class common{
     public static void main(String[] args){
 
-    	   System.out.println("共通ファイル１だよ");        }
+    	   System.out.println("共通ファイル１だよ。A1で編集しちゃったよ");        }
 }
