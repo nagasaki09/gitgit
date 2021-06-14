9aa67ad6cd03b7c08de075bbe23db54b01b74abb
nagasaki <nagasaki@atf.co.jp>
2021-06-14 23:18:43 +0900

    update A2 1!!!!!

--- zikkenn1/test1.java
+++ zikkenn1/test1.java
@@ -1,5 +1,6 @@
 public class test1{
     public static void main(String[] args){
 
-    	   System.out.println("共通ブランチ追加したよ");        }
+    	   System.out.println("共通ブランチ追加したよ"); 
+    	   System.out.println("A2ブランチ追加したよ");        }
 }
